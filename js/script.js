0e088eac0d4d38f61d69323140440f2c07195876
jackiesd <sdjacqueline@gmail.com>
2015-01-16 01:18:17 +0100

    css, js Folder Updated

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 var main = function() {
 	 $("#header").fadeTo("slow", 1);
 
-	$("#nav .web").click(function() {
-		$("#work").addClass("hidden");
-		$(".web").removeClass("hidden");
+	$(".nav .web ").click(function() {
+		$(".work .grafics").addClass("hidden");
+		$(".work .drawings").addClass("hidden");
+
+		$(".work .web").removeClass("hidden");
 	});
 
-	$("#nav .grafics").click(function() {
-		$("#work").addClass("hidden");
-		$(".grafics").removeClass("hidden");
+	$(".nav .grafics").click(function() {
+		$(".work .web").addClass("hidden");
+		$(".work .drawings").addClass("hidden");
+
+		$(".work .grafics").removeClass("hidden");
 	});
 
-	$("#nav .drawings").click(function() {
-		$("#work").addClass("hidden");
-		$(".drawings").removeClass("hidden");
+	$(".nav .drawings").click(function() {
+		$(".work .grafics").addClass("hidden");
+		$(".work .web").addClass("hidden");
+
+		$(".work .drawings").removeClass("hidden");
 	});
 
+	/* $("img").click(function() {
+		$(this).fadeOut(300);
+		$(".home").addClass("newbg");
+	}); */
 };
 
 $(document).ready(main);
