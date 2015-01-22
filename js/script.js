ec1f7986966bd47a72eea60332eafc9dc8953da1
jackiesd <sdjacqueline@gmail.com>
2015-01-23 00:33:39 +0100

    new box.css, style.css and script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
+var screenHeight = screen.height;
+var screenWidth = screen.width;
+
 var main = function() {
-	 $("#header").fadeTo("slow", 1);
-
-	$(".nav .web ").click(function() {
+	
+	$("#nav .web_nav ").click(function() {
 		$(".work .grafics").addClass("hidden");
 		$(".work .drawings").addClass("hidden");
-
 		$(".work .web").removeClass("hidden");
+
+		
+	//	$(".work .web").slideToggle("slow");
 	});
 
-	$(".nav .grafics").click(function() {
+	$("#nav .grafics_nav").click(function() {
 		$(".work .web").addClass("hidden");
 		$(".work .drawings").addClass("hidden");
-
 		$(".work .grafics").removeClass("hidden");
+	
+		//$(".work .grafics").slideDown("slow");
 	});
 
-	$(".nav .drawings").click(function() {
+	$("#nav .drawings_nav").click(function() {
 		$(".work .grafics").addClass("hidden");
 		$(".work .web").addClass("hidden");
-
 		$(".work .drawings").removeClass("hidden");
+		
+		//$(".work .drawings").slideToggle("slow");
 	});
 
-	/* $("img").click(function() {
-		$(this).fadeOut(300);
-		$(".home").addClass("newbg");
-	}); */
+
+
+	/*$(".work img").hover(function(){
+		$(this).css("box-shadow", "10px 10px 20px #aaa");
+	},	function() {
+		$(this).css("box-shadow", "10px 10px 20px #ccc");
+	}); 
+
+
+
+	if(screenHeight > 800) {
+		$("#header").css("height", "380px");
+	}
+
+	if(screenWidth < 1400) {
+		$(".container").css("width", "1200px");
+	} */
+	
+
+
 };
 
 $(document).ready(main);
